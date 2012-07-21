0c9e328a729a3793a8767d3d6333b65b88827236
minh ta <snypergtr@gmail.com>
2012-07-21 16:52:04 -0700

    ui changes

---
+++ js/minh.js
@@ -0,0 +1,13 @@
+function addEventSimple(obj,evt,fn) {
+	if (obj.addEventListener)
+		obj.addEventListener(evt,fn,false);
+	else if (obj.attachEvent)
+		obj.attachEvent('on'+evt,fn);
+}
+
+function removeEventSimple(obj,evt,fn) {
+	if (obj.removeEventListener)
+		obj.removeEventListener(evt,fn,false);
+	else if (obj.detachEvent)
+		obj.detachEvent('on'+evt,fn);
+}
